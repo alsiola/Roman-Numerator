996813ed9504f956882945d94644d0c81e6415d9
Alex Young <alex@alexyoung.info>
2017-01-25 16:22:59 +0000

    --remove dotenv

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-require('dotenv').load();
-
 app.use(express.static('build'));
 
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
